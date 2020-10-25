bda3a04ca6677506a2918657c14efbc4bd6d90a4
Brandon Brooks <brooksbrandon81@gmail.com>
2020-10-25 11:46:54 -0400

    Recursion works for Morse To Ascii
    Added some functionality to code to make recursive

--- MorseCode.s
+++ MorseCode.s
@@ -5,16 +5,17 @@ string1: .asciiz "Enter a Character: "
 string2: .asciiz "Enter a Pattern: "
 string3: .asciiz "Morse Code: "
 string4: .asciiz "ASCII: "
-string5: .asciiz "End of Program\n"
+string5: .asciiz "\nEnd of Program\n"
 string6: .asciiz "[Error] no ASCII2MC!\n"
 string7: .asciiz "[Error] no MC2ASCII!\n"
 string8: .asciiz "[Error] Invalid combination!\n"
 string9: .asciiz "Dot\n"
 string10: .asciiz "Dash!\n"
 string11: .asciiz "Compare\n"
-string12: .asciiz "They Equal\n"
+string12: .asciiz "Space\n"
 stringvals: .asciiz "-. "
 endLine: .asciiz "\n"
+decodedAscii: .space 128
 
 dict: .word 0x55700030, 0x95700031, 0xA5700032, 0xA9700033, 0xAA700034, 0xAAB00035, 0x6AB00036, 0x5AB00037, 0x56B00038, 0x55B00039, 0x9C000041, 0x6AC00042, 0x66C00043, 0x6B000044, 0xB0000045, 0xA6C00046, 0x5B000047, 0xAAC00048, 0xAC000049, 0x95C0004A, 0x6700004B, 0x9AC0004C, 0x5C00004D, 0x6C00004E, 0x5700004F, 0x96C00050, 0x59C00051, 0x9B000052, 0xAB000053, 0x70000054, 0xA7000055, 0xA9C00056, 0x97000057, 0x69C00058, 0x65C00059, 0x5AC0005A
 s_dsh: .byte '-'
@@ -127,21 +128,24 @@ MC2A:  #Here we get the buffer we want to read from and print some stuff
   syscall 
 
 
-
-li $s0, 0 #value
-li $t4, 0 #counter
+li $s1, 0 #Global Counter
+li $s2, 0 #End Flag if one no space quit
+Restart:
+  li $s0, 0 #value
+  li $t4, 0 #counter
 CheckDash:
   #This has to take in the dashes and dots, assign bit values based on input
   la $t0, buffer    #Load buffer into memory
   la $t1, stringvals #Load 3 options into memory
-  add $t0, $t0, $t4  #Add counter to buffer address to get index
+  add $t0, $t0, $s1  #Add counter to buffer address to get index
   lb $t2, 0($t0) #Load into memory
   lb $t3, 0($t1)
   bne $t2, $t3, CheckDot #If not equal see if it is dot
   li $v0, 4  
   la $a0, string10
-  syscall
+  #syscall
   addi $t4, $t4, 1 #Increment counter
+  addi $s1, $s1, 1
   sll $s0, $s0, 2  #shifts and adds the value of the dot
   addi $s0,$s0, 1
   j CheckDash #Recursion
@@ -151,24 +155,27 @@ CheckDot:
   bne $t2, $t3, CheckSpace #If not equal check space
   li $v0, 4
   la $a0, string9
-  syscall
+  #syscall
   addi $t4, $t4, 1  #Increment
+  addi $s1, $s1, 1
   sll $s0, $s0, 2
   addi $s0,$s0, 2
   j CheckDash  #Recursion
   
 CheckSpace:
   lb $t3, 2($t1)  #Load third option into memory
-  bne $t2, $t3, EndCode #If not equal exit
+  bne $t2, $t3, SetFlag #If not equal exit
   li $v0, 4
   la $a0, string12
-  syscall
-  addi $t4, $t4, 1  #Increment
-  j CheckDash #Recursion
+  #syscall
+  #j CheckDash #Recursion
+
+
 EndCode:
-  sll $s0, $s0, 2  #shifts and adds the value of the dot
+  sll $s0, $s0, 2  #shifts and adds the value of the endcode
   addi $s0,$s0, 3  #puts in endcode
-  addi $t4, $t4, 1
+  addi $t4,$t4,1
+  addi, $s1, $s1, 1
   #HAVE TO SHIFT TO PUT THIS ALL THE WAY TO THE RIGHT
   li $t5, 8
   sub $t5, $t5, $t4  #8 inputs - used inputs = inputs left to shift by
@@ -194,33 +201,13 @@ Iterate:
   li $a0, 0 #load 0
   add $a0, $a0, $t1 #print t1 which is the ascii value for dictionary search that hit
   syscall
-  j EXIT
+  bne $s2 ,$0, EXIT
+  j Restart
 
 
 
 
 
-
-
-li $t2, 0 #loads a counter
-Compare:
-  la $t0, buffer #Loads Address users input
-  la $t1, string11 #Loads Address of string to compare to
-  add $t0, $t0, $t2 #increments the addresses based on iteration
-  add $t1, $t1, $t2
-  lb $t0, 0($t0) #loads values at addresses
-  lb $t1, 0($t1)
-  bne $t0, $t1, EXIT  #checks if they are equal
-  beqz $t1, Nice #If they are equal it goes to label "Nice"
-  addi $t2, $t2, 1
-  j Compare
-
-Nice: 
-  li $v0, 4
-  la $a0, string12
-  syscall
-
-  
 
 #--------------------------------------------------------------#
 #-------------------- Write your code Here --------------------#
@@ -248,3 +235,7 @@ EXIT:
   li $a0, 0
   li $v0, 17              #exit
   syscall
+
+  SetFlag:
+    li $s2, 1
+    j EndCode
